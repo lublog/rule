f87ebcbdfcbe569b958ce7365007bda10ca05b83
luyao <l767475207@gmail.com>
2023-11-16 20:23:22 +0800

    test

--- merge_proxy_rules.py
+++ merge_proxy_rules.py
@@ -1,8 +1,10 @@
 import requests
 
+
 def read_custom_rules(filename):
     with open(filename, 'r') as file:
         return file.read()
+
 
 def merge_proxy_rules(url, custom_rules_str, output_filename):
     try:
@@ -12,10 +14,11 @@ def merge_proxy_rules(url, custom_rules_str, output_filename):
     except Exception as e:
         return f"Error fetching remote rules: {e}"
 
-    merged_rules = remote_rules + '\\n' + custom_rules_str
+    merged_rules = remote_rules + '\n' + custom_rules_str
 
     with open(output_filename, 'w') as file:
         file.write(merged_rules)
+
 
 url = "https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Rule/OpenAI.list"
 custom_rules_str = read_custom_rules('custom_rules.txt')
